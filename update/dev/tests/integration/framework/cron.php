bee2606d4b27706236b30aca9d7201efa2c0633b
Alan Barber <alan@cadence-labs.com>
2016-09-11 20:54:56 -0600

    Initial commit of files

---
+++ update/dev/tests/integration/framework/cron.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+require_once __DIR__ . '/magento_bp.php';
+require_once __DIR__ . '/../../../../cron.php';
---
+++ update/dev/tests/unit/framework/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+require_once __DIR__ . '/../../../../app/bootstrap.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
