1aa26d4f1ee8d446bf532e596b6c2807da42c504
Jay <jay@coretekllc.com>
2017-07-21 12:39:20 -0700

    CDL-7 #comment removed M2 files, moved module to root

--- update/dev/tests/integration/framework/cron.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Copyright © 2016 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
-require_once __DIR__ . '/magento_bp.php';
-require_once __DIR__ . '/../../../../cron.php';
